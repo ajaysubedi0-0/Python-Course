ce02e71ad654a698774aaca7fb0ae72b183b3810
ajaysubedi0-0 <AjaySubedi@protonmail.com>
2024-09-08 09:42:37 +0545

    first commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+firstName = ['Ajay','prajwol','kanaya',]
+surname = ['subedi','adhikari','tamang']
+name = firstName+ surname
+age = 18
+Class ="12"
+print(f"My name is {name[1]} .I read in class {Class} .I am years {age} old")
