3c6b92779eefdbdf44a87305150f605ab48a32f1
ajaysubedi0-0 <AjaySubedi@protonmail.com>
2024-09-08 10:23:10 +0545

    first commit

---
+++ day 2.py
@@ -0,0 +1,10 @@
+friend ={\
+    'name' : 'pranjal',
+    'age' : 22 
+    }
+
+friends = ['manish','mahesh','pranjal']
+friends.append('Anup')
+friends.append('Bhojraj')
+friends.insert(1, 'anuj')
+print(friends[1],friend['name'])
