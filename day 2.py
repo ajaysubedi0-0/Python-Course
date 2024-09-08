57b4fd200d3fc7cf4d995459ed085bc15b20994f
ajaysubedi0-0 <AjaySubedi@protonmail.com>
2024-09-08 15:55:12 +0545

    for day 2

--- day 2.py
+++ day 2.py
@@ -1,10 +1,28 @@
-friend ={\
+friend ={
     'name' : 'pranjal',
-    'age' : 22 
-    }
+    'age' : 22, 
+    'machine' : 'lol'
+   }
 
 friends = ['manish','mahesh','pranjal']
-friends.append('Anup')
-friends.append('Bhojraj')
-friends.insert(1, 'anuj')
-print(friends[1],friend['name'])
+#friends.append('Anup')
+#friends.append('Bhojraj')
+#friends.insert(1, 'anuj')
+#print(friends)
+#print(friend['name'])
+#print(friends[0:4])
+#friends.pop(1) 
+#print(friends)
+#print(len (friends))
+friend['isNepali'] = True
+# del(friend['machine'])
+# print(friend['isNepali'])
+# print(friend.keys())
+# print(friend.values())
+# print(friend)
+# if 'name' in friend:
+#     print("name exists")
+# my_tuple = (1,2,1,4,'me',3.14)
+# print(my_tuple)
+my_set = {1,2,3,3,2}
+print(my_set)
